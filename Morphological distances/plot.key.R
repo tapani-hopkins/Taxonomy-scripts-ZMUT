19fef8687f0acaa3f9c95bc425c23b750a9526cc
tapani-hopkins <tapani-hopkins@users.noreply.github.com>
2017-09-15 14:58:32 +0300

    Updated plot.key

--- Morphological distances/plot.key.R
+++ Morphological distances/plot.key.R
@@ -4,13 +4,14 @@
 # col:  The colours for the lines. Default is a gradient from red to yellow/green
 # show.groups:  Whether to show the group names (shown where the lines branch)
 # show.lines:   Whether to show the lines
+# convert.groups:   Whether to convert group names (i.e. the names or numbers of the steps of the key) to make sure they are unambiguous. Default is "i" ("if needed") = convert if the key is ambiguous; e.g. there are two steps of the key with the same name. Also accepts True = always convert, and False = do not convert. Conversion happens by combining the steps of the key, so that e.g. steps "1, 2, 1" become "1, 1.2, 1.2.1". Usually there is no need to change the default: it ensures that both shorthand (1, 2, 1) and longhand (1, 1.2, 1.2.1) notations for key structure are accepted and give the same result.
 ## Returns a list with two data frames:
 # groups:   the coordinates of the different groups / line branches
 # line.coordinates: the coordinates of the lines (and their suggested colour)
 ## For finer control of the plot, set show.groups and show.lines to False, and draw the lines and groups based on the returned values
 
 
-plot.key = function(p, st, col=NULL, show.groups=T, show.lines=T){
+plot.key = function(p, st, col=NULL, show.groups=T, show.lines=T, convert.groups="i"){
     
     # Check the colours of the lines. If none were given, use default colours.
     # The default is a gradient: first steps of the key are red, last steps green/yellow.
@@ -25,13 +26,53 @@ plot.key = function(p, st, col=NULL, show.groups=T, show.lines=T){
     x = p$sites[,1]
     y = p$sites[,2]
     
-    ## Save the key structure in 'stf'
-    # Combine the steps in 'st' to make sure each row has unique grouping identifiers
-    # i.e. "1.1.2.1" instead of just 1, 1, 2, 1
-    # (the above is unclear, but opening 'st' and 'stf' will clarify what I'm trying to say)
-    stf = st
+    ## Save the key structure into 'stf', either in the original format or (if the key was ambiguous or convert.groups=TRUE) with the steps of the key combined.
+    ## Combining allows giving shorthand notation for the steps of the key:
+    ## e.g. steps "1, 2, 1" can be combined by the script into "1, 1.2, 1.2.1"
+    #
+    # Convert factors and NAs in the key structure to characters
+    st[] = lapply(st, as.character)
+    st[is.na(st)] = "NA"
+    # Create a copy of the key structure with combined group names (i.e. steps of the key)
+    # Each row of 'st' is a step in the key
+    st.combined = st
     for (i in 2:dim(st)[1]){
-        stf[i,] = paste(stf[i-1,], st[i,], sep=".")
+        st.combined[i,] = paste(st.combined[i-1,], st[i,], sep=".")
+    }
+    # Choose either the combined key structure (st.combined) or the original (st) for use by the script.
+    # Save the selected key structure into 'stf'.
+    if ( substr(convert.groups,1,1) == "i" ){
+        # Combine if the given key structure (st) is ambiguous, e.g. if different steps of the key have the same name, or the key's branches join.
+        # (typically this will be because the key is in shorthand: "1, 2, 1" is ambiguous since there are two steps named "1", it should be combined into "1, 1.2, 1.2.1")
+        # In practice this is done by checking, one row at a time, if combining the key makes any difference to its structure. If it does, the key is ambiguous.
+        stf = st
+        for (i in 1:dim(st)[1]){
+            # Compare the two keys one row at a time
+            c1 = st[i,]
+            c2 = st.combined[i,]
+            # Ignore NA values
+            nas = grep("NA",c1)
+            if (length(nas)>0){
+                c1 = c1[-nas]
+                c2 = c2[-nas]
+            }
+            # Check if the structure of this row is the same in both keys
+            # If it is not, the key is ambiguous (since combining it creates a different key structure).
+            c1 = factor(as.character(c1))
+            c2 = factor(as.character(c2))
+            differences = sum( as.numeric(c1) != as.numeric(c2) )
+            if (differences > 0){
+                # Use the combined key if the original is ambiguous
+                stf = st.combined
+            }
+        }
+    } else {
+        # Combine only if asked to do so
+        if (convert.groups){
+            stf = st.combined
+        } else {
+            stf = st
+        }
     }
     
     ## Get the average coordinates of each group
@@ -57,7 +98,9 @@ plot.key = function(p, st, col=NULL, show.groups=T, show.lines=T){
     names(XY) = names(xy)
     row.names(XY) = c("x","y")
     xy = XY
-    xy = xy[,-grep("NA",names(xy))]
+    if (length(grep("NA",names(xy)))>0){
+        xy = xy[,-grep("NA",names(xy))]
+    }
     
     ## Draw the lines, group names etc.
     # Draw separately for each species, starting from the first step
@@ -72,7 +115,7 @@ plot.key = function(p, st, col=NULL, show.groups=T, show.lines=T){
             xy1 = xy[, names(xy)==stf[r,sp] ]   # end point of this line
             # Get the line start and end points, and the line colours
             # Ignore NA values, e.g. only do something if the line continues = haven't reached an end point of the key
-            if( !is.na(st[r,sp]) ){
+            if( st[r,sp] != "NA" ){
                 xy.lines = rbind(xy.lines, c(xy0[1], xy1[1], xy0[2], xy1[2]))
                 cols = c(cols, col[r-1])
             }
